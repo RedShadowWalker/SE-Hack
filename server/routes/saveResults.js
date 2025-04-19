b78038e51cb960adf999286fbe8afa6f68dedc11
Divit Gupta <divit.gupta23@spit.ac.in>
2025-04-19 16:50:21 +0530

    Quiz config 1

---
+++ server/routes/saveResults.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const QuizResult = require("../models/QuizResult");
+
+router.post("/save-results", async (req, res) => {
+  try {
+    const { quizId, results } = req.body;
+    const doc = new QuizResult({ quizId, results });
+    await doc.save();
+    res.send("Saved to MongoDB");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+module.exports = router;
