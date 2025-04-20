af996dde5b5c64b13d7ff0892fa8b2fc99f18cb5
Madhura Lolayekar <madhura.lolayekar23@spit.ac.in>
2025-04-20 10:56:14 +0530

    Final

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 //import Login from "./Login.jsx";
-//import { ClerkProvider } from "@clerk/clerk-react";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
